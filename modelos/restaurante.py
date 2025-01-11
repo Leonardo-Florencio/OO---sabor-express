ab3b729368c4b299d1f02a57c898e9fa1da23ac2
Leonardo-Florencio <leonardo_florenciop@outlook.com>
2025-01-11 13:09:37 -0300

    utilização de construtor, definindo atributos com 'def __init__'

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,17 +1,22 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
-    
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Bistrô'
-restaurante_praca.categoria = 'Italiana'
-
-restaurante_pizza = Restaurante()
-restaurante_pizza.nome = 'Pizza Place'
-restaurante_pizza.categoria = 'Fast Food'
-restaurante_pizza.ativo = True
+    #utilizar o def __init__ para ser um construtor - Ou seja, quando o objeto for definido, deve ter as próprias informações
+    #def é usado para definir o init como construtor
+    #self é para garantir que o objeto está sendo referenciado como ele mesmo
+    #nome e categoria, nesse caso, se tornam os requisitos obrigatórios a serem definidos
+    #Dessa forma, se tentar criar um objeto sem essas informações, o código vai retornar um erro esperando por essa informação
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        
+#na hora de declarar o objeto, basta colocar seus atributos na ordem em que foram definidos no self do construtor
+restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(f'{restaurante_praca.nome} e {restaurante_praca.categoria}')
+#Ao usar o método dir() conseguimos enxergar todas as informações relacionados ao objeto da classe, desde os métodos padrão até os atributos definidos pelo desenvolvedor
+#Ao usar o método vars() conseguimos enxergar todas as informações contidas nos atributos definidos
+
+print(vars(restaurante_praca))
+print(vars(restaurante_pizza))
