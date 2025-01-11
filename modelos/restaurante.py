cc2ade3c69f19c329101b210d3986f702bb9f6dc
Leonardo-Florencio <leonardo_florenciop@outlook.com>
2025-01-11 14:01:44 -0300

    criação de método próprio para listar as instâncias criadas

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,4 +1,5 @@
 class Restaurante:
+    restaurantes = []
     #utilizar o def __init__ para ser um construtor - Ou seja, quando o objeto for definido, deve ter as próprias informações
     #def é usado para definir o init como construtor
     #self é a referência à instância utilizada no momento, ou seja, permite entender que é daquela instância. É o padrão utilizado em python
@@ -8,21 +9,23 @@ class Restaurante:
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+        Restaurante.restaurantes.append(self) #método append utilizado para que, cada instância da classe quando criada, seja adicionada no array restaurantes[]
     # utilizado o método __str__ para que ele apresente o objeto em forma de texto. Ele deve receber um retorno, que irá buscar as informações dos atributos daquela instância para devolver em formato de texto 
     # como o método retorna string, é possível utilizar f-string para "estilizar" o retorno do texto 
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes(): #método criado para listar os restaurantes contidos no array restaurantes[]. O for procura por restaurante dentro do array da classe e, para cada um, printa seu nome, categoria e ativo no f'string definido
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
         
 #na hora de declarar o objeto, basta colocar seus atributos na ordem em que foram definidos no self do construtor
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
-
 #Ao usar o método dir() conseguimos enxergar todas as informações relacionados ao objeto da classe, desde os métodos padrão até os atributos definidos pelo desenvolvedor
 #Ao usar o método vars() conseguimos enxergar todas as informações contidas nos atributos definidos
 
-print(restaurante_praca)
-print(restaurante_pizza)
+Restaurante.listar_restaurantes()
 
 
