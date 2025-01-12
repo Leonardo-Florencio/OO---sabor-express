b1daf6f191ff00212388b7d3170c611ad99d1ee0
Leonardo-Florencio <leonardo_florenciop@outlook.com>
2025-01-12 15:18:23 -0300

    import da classe base, para utilização de herança

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,3 +1,6 @@
-class Prato:
+from modelos.cardapio.item_cardapio import ItemCardapio
+
+class Prato(ItemCardapio): #colocando a classe mãe "ItemCardápio" no argumento dessa classe, garante que a classe Prato receba herança da classe mãe, conectando uma a outra
     def __init__(self, nome, preco, descricao):
-        pass
+        super().__init__(nome, preco) #super é um objeto especial, que permite acessar os atributos de outra classe a partir de uma classe diferente
+        self.descricao = descricao
