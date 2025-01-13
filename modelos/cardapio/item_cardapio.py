cca97c775639b7cf061f5d8f99f2a23ec9dcad5f
Leonardo-Florencio <leonardo_florenciop@outlook.com>
2025-01-13 08:23:06 -0300

    explicação de polimorfismo no comentário

--- modelos/cardapio/item_cardapio.py
+++ modelos/cardapio/item_cardapio.py
@@ -6,6 +6,7 @@ class ItemCardapio(ABC): #necessário colocar o ABC no argumento, para que o mé
         self._preco = preco
         
     #criação de classe abstrata - Siginifica que todas as classes derivadas precisam ter esse método, mas a classe não precisa saber como ele funciona. Esses detalhes são definidos nas classes que recebem herança dessa classe
+    # essa habilidade de um método poder ser diferente em diferentes classes se chama polimorfismo
     @abstractmethod
     def aplicar_desconto(self):
         pass
