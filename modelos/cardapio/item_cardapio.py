c2a5c6f37597df8ba9918bdea91acf09c3ce9a1b
Leonardo-Florencio <leonardo_florenciop@outlook.com>
2025-01-13 08:19:47 -0300

    criação de método abstrato para criação de desconto nas classes filhas de item_cardapio

--- modelos/cardapio/item_cardapio.py
+++ modelos/cardapio/item_cardapio.py
@@ -1,4 +1,11 @@
-class ItemCardapio:
+from abc import ABC, abstractmethod #import necessário para criação de classe abstrata
+
+class ItemCardapio(ABC): #necessário colocar o ABC no argumento, para que o método abstrato possa ser utilizado
     def __init__(self, nome, preco):
         self._nome = nome
         self._preco = preco
+        
+    #criação de classe abstrata - Siginifica que todas as classes derivadas precisam ter esse método, mas a classe não precisa saber como ele funciona. Esses detalhes são definidos nas classes que recebem herança dessa classe
+    @abstractmethod
+    def aplicar_desconto(self):
+        pass
